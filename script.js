e9d9573bb144245ccd6fc8d2d5d265bd2896bb45
Nivaldo <nivaldo_couto@hotmaoil.com>
2021-05-24 14:13:16 -0300

    add arquivo script no projeto

---
+++ script.js
@@ -0,0 +1,51 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO], Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        res.innerHTML = `Idade calculada ${idade}`
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'img/bebe_m.png')
+            } else if (idade < 21) {
+                // Jovem
+                img.setAttribute('src', 'img/jovem_m.png')
+            } else if (idade < 60) {
+                // Adulto
+                img.setAttribute('src', 'img/adulto_m.png')
+            }else {
+                // Idoso
+                img.setAttribute('src', 'img/idoso_m.png')
+            }
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'img/bebe_f.png')
+            } else if (idade < 21) {
+                // Jovem
+                img.setAttribute('src', 'img/jovem_f.png')
+            } else if (idade < 60) {
+                // Adulto
+                img.setAttribute('src', 'img/adulto_f.png')
+            }else {
+                // Idoso
+                img.setAttribute('src', 'img/idosa_f.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        img.style.paddingTop= '15px'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
